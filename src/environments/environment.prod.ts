297e31f20f8915403525d540c3cc4172c1f116d6
lestersantiago <lester.santiago@hotmail.com>
2017-11-01 18:07:23 -0700

    udemy section 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBqDOlZtASQ-WNv60wb7RRDBpMj8O_fHPc",
+    authDomain: "oshop-8449c.firebaseapp.com",
+    databaseURL: "https://oshop-8449c.firebaseio.com",
+    projectId: "oshop-8449c",
+    storageBucket: "",
+    messagingSenderId: "780567517122"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBqDOlZtASQ-WNv60wb7RRDBpMj8O_fHPc",
+    authDomain: "oshop-8449c.firebaseapp.com",
+    databaseURL: "https://oshop-8449c.firebaseio.com",
+    projectId: "oshop-8449c",
+    storageBucket: "",
+    messagingSenderId: "780567517122"
+  }
 };
